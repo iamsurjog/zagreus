d4eec3ab1621b5aee4105cb28aa3b9a90a261ff7
iamsurjog <iamsurjog@gmail.com>
2026-03-28 02:33:59 +0530

    fix(title): title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -27,7 +27,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
                 content: 'width=device-width, initial-scale=1',
             },
             {
-                title: 'Hermes | SEDSVIT',
+                title: 'Erebus | SEDSVIT',
             },
         ],
         links: [
